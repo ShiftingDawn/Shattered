54a3327e470d22c44c4bcd629e333ec773e2f828
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2024-11-12 18:25:44 +0100

    Add lwjgl demo

--- core/src/main/java/shattered/Shattered.java
+++ core/src/main/java/shattered/Shattered.java
@@ -1,11 +1,54 @@
 package shattered;
 
 import java.io.File;
+import java.nio.FloatBuffer;
 import lombok.Getter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.joml.Matrix4f;
+import org.joml.Quaternionf;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL;
 import shattered.core.event.EventBusImpl;
 import shattered.lib.Internal;
+import shattered.lib.gfx.Display;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_CULL_FACE;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_FILL;
+import static org.lwjgl.opengl.GL11.GL_FRONT_AND_BACK;
+import static org.lwjgl.opengl.GL11.GL_LINE;
+import static org.lwjgl.opengl.GL11.GL_LINES;
+import static org.lwjgl.opengl.GL11.GL_POLYGON_OFFSET_LINE;
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glDisable;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glPolygonMode;
+import static org.lwjgl.opengl.GL11.glPolygonOffset;
+import static org.lwjgl.opengl.GL11.glVertex3f;
+import static org.lwjgl.opengl.GL11.glViewport;
+import static org.lwjgl.opengl.GL20.GL_FRAGMENT_SHADER;
+import static org.lwjgl.opengl.GL20.GL_VERTEX_SHADER;
+import static org.lwjgl.opengl.GL20.glAttachShader;
+import static org.lwjgl.opengl.GL20.glCompileShader;
+import static org.lwjgl.opengl.GL20.glCreateProgram;
+import static org.lwjgl.opengl.GL20.glCreateShader;
+import static org.lwjgl.opengl.GL20.glGetUniformLocation;
+import static org.lwjgl.opengl.GL20.glLinkProgram;
+import static org.lwjgl.opengl.GL20.glShaderSource;
+import static org.lwjgl.opengl.GL20.glUniform3f;
+import static org.lwjgl.opengl.GL20.glUniformMatrix4fv;
+import static org.lwjgl.opengl.GL20.glUseProgram;
 
 public final class Shattered {
 
@@ -25,9 +68,140 @@ public final class Shattered {
 
 	private void init() {
 		EventBusImpl.init();
+		Display.init();
 	}
 
+	Matrix4f viewProjMatrix = new Matrix4f();
+	FloatBuffer fb = BufferUtils.createFloatBuffer(16);
+
 	private void start() {
+		glfwMakeContextCurrent(Display.getWindow());
+		glfwSwapInterval(0);
+		GL.createCapabilities();
+
+		glClearColor(0.6f, 0.7f, 0.8f, 1.0f);
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_CULL_FACE);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+		// Create a simple shader program
+		final int program = glCreateProgram();
+		final int vs = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vs, """
+				uniform mat4 viewProjMatrix;
+				void main(void) {
+				    gl_Position = viewProjMatrix * gl_Vertex;
+				}
+				""");
+		glCompileShader(vs);
+		glAttachShader(program, vs);
+		final int fs = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fs, """
+				uniform vec3 color;
+				void main(void) {
+					gl_FragColor = vec4(color, 1.0);
+				}
+				""");
+		glCompileShader(fs);
+		glAttachShader(program, fs);
+		glLinkProgram(program);
+		glUseProgram(program);
+
+		// Obtain uniform location
+		final int matLocation = glGetUniformLocation(program, "viewProjMatrix");
+		final int colorLocation = glGetUniformLocation(program, "color");
+		long lastTime = System.nanoTime();
+
+		/* Quaternion to rotate the cube */
+		final Quaternionf q = new Quaternionf();
+
+		while (!GLFW.glfwWindowShouldClose(Display.getWindow())) {
+			final long thisTime = System.nanoTime();
+			final float dt = (thisTime - lastTime) / 1E9f;
+			lastTime = thisTime;
+
+			glViewport(0, 0, Display.getWidth(), Display.getHeight());
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			// Create a view-projection matrix
+			this.viewProjMatrix.setPerspective((float) Math.toRadians(45.0f),
+							(float) Display.getWidth() / Display.getHeight(), 0.01f, 100.0f)
+					.lookAt(0.0f, 4.0f, 10.0f,
+							0.0f, 0.5f, 0.0f,
+							0.0f, 1.0f, 0.0f);
+			// Upload the matrix stored in the FloatBuffer to the
+			// shader uniform.
+			glUniformMatrix4fv(matLocation, false, this.viewProjMatrix.get(this.fb));
+			// Render the grid without rotating
+			glUniform3f(colorLocation, 0.3f, 0.3f, 0.3f);
+			this.renderGrid();
+
+			// rotate the cube (45 degrees per second)
+			// and translate it by 0.5 in y
+			this.viewProjMatrix.translate(0.0f, 0.5f, 0.0f)
+					.rotate(q.rotateY((float) Math.toRadians(45) * dt).normalize());
+			// Upload the matrix
+			glUniformMatrix4fv(matLocation, false, this.viewProjMatrix.get(this.fb));
+
+			// Render solid cube with outlines
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			glUniform3f(colorLocation, 0.6f, 0.7f, 0.8f);
+			this.renderCube();
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			glEnable(GL_POLYGON_OFFSET_LINE);
+			glPolygonOffset(-1.f, -1.f);
+			glUniform3f(colorLocation, 0.0f, 0.0f, 0.0f);
+			this.renderCube();
+			glDisable(GL_POLYGON_OFFSET_LINE);
+
+			glfwSwapBuffers(Display.getWindow());
+			glfwPollEvents();
+		}
+	}
+
+	void renderCube() {
+		glBegin(GL_QUADS);
+		glVertex3f(0.5f, -0.5f, -0.5f);
+		glVertex3f(-0.5f, -0.5f, -0.5f);
+		glVertex3f(-0.5f, 0.5f, -0.5f);
+		glVertex3f(0.5f, 0.5f, -0.5f);
+
+		glVertex3f(0.5f, -0.5f, 0.5f);
+		glVertex3f(0.5f, 0.5f, 0.5f);
+		glVertex3f(-0.5f, 0.5f, 0.5f);
+		glVertex3f(-0.5f, -0.5f, 0.5f);
+
+		glVertex3f(0.5f, -0.5f, -0.5f);
+		glVertex3f(0.5f, 0.5f, -0.5f);
+		glVertex3f(0.5f, 0.5f, 0.5f);
+		glVertex3f(0.5f, -0.5f, 0.5f);
+
+		glVertex3f(-0.5f, -0.5f, 0.5f);
+		glVertex3f(-0.5f, 0.5f, 0.5f);
+		glVertex3f(-0.5f, 0.5f, -0.5f);
+		glVertex3f(-0.5f, -0.5f, -0.5f);
+
+		glVertex3f(0.5f, 0.5f, 0.5f);
+		glVertex3f(0.5f, 0.5f, -0.5f);
+		glVertex3f(-0.5f, 0.5f, -0.5f);
+		glVertex3f(-0.5f, 0.5f, 0.5f);
+
+		glVertex3f(0.5f, -0.5f, -0.5f);
+		glVertex3f(0.5f, -0.5f, 0.5f);
+		glVertex3f(-0.5f, -0.5f, 0.5f);
+		glVertex3f(-0.5f, -0.5f, -0.5f);
+		glEnd();
+	}
+
+	void renderGrid() {
+		glBegin(GL_LINES);
+		for (int i = -20; i <= 20; i++) {
+			glVertex3f(-20.0f, 0.0f, i);
+			glVertex3f(20.0f, 0.0f, i);
+			glVertex3f(i, 0.0f, -20.0f);
+			glVertex3f(i, 0.0f, 20.0f);
+		}
+		glEnd();
 	}
 
 	public void stop() {
---
+++ core/src/main/java/shattered/lib/gfx/Display.java
@@ -0,0 +1,104 @@
+package shattered.lib.gfx;
+
+import java.nio.IntBuffer;
+import lombok.Getter;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWFramebufferSizeCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_SAMPLES;
+import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetFramebufferSizeCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.nglfwGetFramebufferSize;
+import static org.lwjgl.system.MemoryUtil.NULL;
+import static org.lwjgl.system.MemoryUtil.memAddress;
+
+public final class Display {
+
+	@Getter
+	private static long window;
+	@Getter
+	private static int width = 800;
+	@Getter
+	private static int height = 600;
+
+	private static GLFWErrorCallback errorCallback;
+	private static GLFWKeyCallback keyCallback;
+	private static GLFWFramebufferSizeCallback fbCallback;
+
+	public static void init() {
+		glfwSetErrorCallback(Display.errorCallback = GLFWErrorCallback.createPrint(System.err));
+		if (!glfwInit()) {
+			throw new IllegalStateException("Unable to initialize GLFW");
+		}
+
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+		glfwWindowHint(GLFW_SAMPLES, 8);
+
+		Display.window = glfwCreateWindow(Display.width, Display.height, "Hello shaders!", NULL, NULL);
+		if (Display.window == NULL) {
+			throw new RuntimeException("Failed to create the GLFW window");
+		}
+
+		glfwSetKeyCallback(Display.window, Display.keyCallback = new GLFWKeyCallback() {
+
+			@Override
+			public void invoke(final long window, final int key, final int scancode, final int action, final int mods) {
+				if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+					glfwSetWindowShouldClose(window, true);
+				}
+			}
+		});
+		glfwSetFramebufferSizeCallback(Display.window, Display.fbCallback = new GLFWFramebufferSizeCallback() {
+
+			@Override
+			public void invoke(final long window, final int w, final int h) {
+				if (w > 0 && h > 0) {
+					Display.width = w;
+					Display.height = h;
+				}
+			}
+		});
+
+		final GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		glfwSetWindowPos(Display.window, (vidmode.width() - Display.width) / 2, (vidmode.height() - Display.height) / 2);
+		glfwShowWindow(Display.window);
+
+		final IntBuffer framebufferSize = BufferUtils.createIntBuffer(2);
+		nglfwGetFramebufferSize(Display.window, memAddress(framebufferSize), memAddress(framebufferSize) + 4);
+		Display.width = framebufferSize.get(0);
+		Display.height = framebufferSize.get(1);
+	}
+
+	public static void destroy() {
+		glfwDestroyWindow(Display.window);
+		Display.keyCallback.free();
+		Display.fbCallback.free();
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();
+	}
+
+	private Display() {
+	}
+}
