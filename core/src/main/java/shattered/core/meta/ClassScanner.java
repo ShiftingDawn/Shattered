066da374e60eb854aa08906f8fd539a0262a58b1
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2024-11-12 17:53:23 +0100

    ClassScanner is now async

--- core/src/main/java/shattered/core/meta/ClassScanner.java
+++ core/src/main/java/shattered/core/meta/ClassScanner.java
@@ -5,11 +5,19 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
+import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
 import it.unimi.dsi.fastutil.objects.ObjectArraySet;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Opcodes;
@@ -19,28 +27,57 @@ import static shattered.Shattered.LOGGER;
 final class ClassScanner {
 
 	private static final Set<String> IGNORED_NAMES = Set.of("package-info.class", "module-info.class");
+	private static final ExecutorService EXECUTOR_SERVICE = Executors.newVirtualThreadPerTaskExecutor();
+	private static final List<CompletableFuture<ClassNode>> RESOLVED = Collections.synchronizedList(new ArrayList<>());
 
 	public static Set<ClassNode> readAllClasses(final File file) throws IOException {
+		ClassScanner.RESOLVED.clear();
 		if (!file.exists()) {
 			throw new FileNotFoundException(file.getAbsolutePath());
 		}
-		final Map<String, ClassNode> classMap = file.isDirectory() ? ClassScanner.loadDirContents(file) : ClassScanner.loadJarContents(file);
-		return new ObjectArraySet<>(classMap.values());
+		if (file.isDirectory()) {
+			ClassScanner.loadDirContents(file);
+		} else {
+			ClassScanner.loadJarContents(file);
+		}
+		try {
+			final Map<String, ClassNode> map = new Object2ObjectArrayMap<>();
+			CompletableFuture.allOf(ClassScanner.RESOLVED.toArray(CompletableFuture[]::new)).get();
+			ClassScanner.RESOLVED.forEach(future -> {
+				try {
+					final ClassNode node = future.get();
+					map.put(node.name, node);
+				} catch (final InterruptedException | ExecutionException e) {
+					throw new RuntimeException(e);
+				}
+			});
+			ClassScanner.RESOLVED.clear();
+			return new ObjectArraySet<>(map.values());
+		} catch (InterruptedException | ExecutionException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
-	private static Map<String, ClassNode> loadDirContents(final File directory) throws IOException {
+	private static void loadDirContents(final File directory) throws IOException {
 		final Map<String, ClassNode> result = new HashMap<>();
 		for (final File file : directory.listFiles()) {
 			final String name = file.getName();
 			if (file.isDirectory()) {
-				result.putAll(ClassScanner.loadDirContents(file));
+				ClassScanner.loadDirContents(file);
 			} else if (name.endsWith(".class") && ClassScanner.IGNORED_NAMES.stream().noneMatch(name::equals)) {
-				final ClassNode node = ClassScanner.readNode(Files.readAllBytes(file.toPath()));
-				result.put(node.name, node);
-				LOGGER.trace("\tFound class: {}", node.name);
+				final CompletableFuture<ClassNode> future = CompletableFuture.supplyAsync(() -> {
+					final ClassNode node;
+					try {
+						node = ClassScanner.readNode(Files.readAllBytes(file.toPath()));
+					} catch (final IOException e) {
+						throw new RuntimeException(e);
+					}
+					LOGGER.trace("\tFound class: {}", node.name);
+					return node;
+				}, ClassScanner.EXECUTOR_SERVICE);
+				ClassScanner.RESOLVED.add(future);
 			}
 		}
-		return result;
 	}
 
 	private static Map<String, ClassNode> loadJarContents(final File jarFile) throws IOException {
@@ -50,9 +87,13 @@ final class ClassScanner {
 			while ((entry = input.getNextJarEntry()) != null) {
 				final String name = entry.getRealName();
 				if (!entry.isDirectory() && name.endsWith(".class") && ClassScanner.IGNORED_NAMES.stream().noneMatch(name::endsWith)) {
-					final ClassNode node = ClassScanner.readNode(input.readAllBytes());
-					result.put(node.name, node);
-					LOGGER.trace("\tFound class: {}", node.name);
+					final byte[] bytes = input.readAllBytes();
+					final CompletableFuture<ClassNode> future = CompletableFuture.supplyAsync(() -> {
+						final ClassNode node = ClassScanner.readNode(bytes);
+						LOGGER.trace("\tFound class: {}", node.name);
+						return node;
+					}, ClassScanner.EXECUTOR_SERVICE);
+					ClassScanner.RESOLVED.add(future);
 				}
 			}
 		}
